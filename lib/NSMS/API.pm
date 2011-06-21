886de64ab31a774704ffbfb69de88b467610fa59
Thiago Berlitz Rondon <thiago@aware.com.br>
2011-06-21 14:05:25 +0000

    Import of TBR/NSMS-API-0.005 from CPAN.
    gitpan-cpan-distribution: NSMS-API
    gitpan-cpan-version:      0.005
    gitpan-cpan-path:         TBR/NSMS-API-0.005.tar.gz
    gitpan-cpan-author:       TBR
    gitpan-cpan-maturity:     released

--- lib/NSMS/API.pm
+++ lib/NSMS/API.pm
@@ -14,7 +14,7 @@ use JSON;
 use utf8;
 
 # ABSTRACT: API para enviar SMS atrav&eacute;s da NSMS (http://www.nsms.com.br/)
-our $VERSION = '0.003'; # VERSION
+our $VERSION = '0.005'; # VERSION
 
 
 has ua => (
@@ -43,6 +43,13 @@ has baseurl => (
     is      => 'rw',
     isa     => 'Str',
     default => 'http://api.nsms.com.br/api',
+);
+
+
+has extra => (
+    is => 'rw',
+    isa => 'Str',
+    default => ''
 );
 
 
@@ -90,7 +97,9 @@ has url_sendsms => (
             . '?to=55'
             . $self->to
             . '&content='
-            . uri_escape( $self->text );
+            . uri_escape( $self->text )
+            . '&extra='
+            . uri_escape ( $self->extra );
 
     }
 );
@@ -145,7 +154,7 @@ NSMS::API - API para enviar SMS atrav&eacute;s da NSMS (http://www.nsms.com.br/)
 
 =head1 VERSION
 
-version 0.003
+version 0.005
 
 =head1 SYNOPSIS
 
@@ -166,7 +175,17 @@ version 0.003
 
 =head1 DESCRIÇÃO
 
-NSMS::API é uma simples API para enviar sms através da plataforma oferecida pela NSMS, com este módulo você pode usufruir de pequenas operações para facilitar a integração com o seu sistema de forma rápida.
+HTTP API é a forma mais popular entre os desenvolvedores quando querem
+efetuar integraçõe utilizando uma API, por que existem várias maneiras de se
+utilizar, facilitadores e módulos disponiveis nas diversas linguagens,
+software e etc. Ela pode ser utilizada tanto com um baixo, como com um alto
+volume de mensagens.
+
+Esta é uma implementação na linguagem Perl da comunicação via SMS, e para
+utilizar ela, basta ter uma conta na NSMS (http://www.nsms.com.br).
+
+A documentação completa desta API esta disponível em:
+L<http://www.nsms.com.br/doc/NSMS_Especificacao_HTTP_API.pdf>
 
 Para mais informações sobre a empresa e o produto, veja L<http://www.nsms.com.br>
 
@@ -187,6 +206,11 @@ Senha NSMS.
 =head2 baseurl
 
 URL para requisição na NSMS, não há por que alterar este atributo a não ser que você tenha certeza do que esteja fazendo.
+
+=head2 extra
+
+Informação adicionar para ser inserida no histórico da mensagem, geralmente utilizado
+para efetuar centro de custo.
 
 =head2 debug
 
